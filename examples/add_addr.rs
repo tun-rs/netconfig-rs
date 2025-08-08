a076e85565a06e45f18d5662d0df1cdb770d4d6d
xmh0511 <970252187@qq.com>
2025-08-08 11:45:39 +0800

    update example

--- examples/add_addr.rs
+++ examples/add_addr.rs
@@ -2,8 +2,8 @@ use std::error::Error;
 use std::net::{IpAddr, Ipv4Addr};
 use netconfig_rs::{Interface, ipnet};
 fn main()->Result<(),Box<dyn Error>>{
-    let interface_index = 0;
-    let interface = unsafe {Interface::from_index_unchecked(interface_index)};
+    let interface_name = "tun0";
+    let interface = Interface::try_from_name(interface_name)?;
     let addrs = interface.addresses()?;
     println!("before {:#?}", addrs);
     interface.add_address(ipnet::IpNet::new(Ipv4Addr::from([10,6,0,1]).into(),24)?)?;
