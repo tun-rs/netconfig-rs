8bcf97674a25dd291eb275f5dbbaeaf44483b74f
xmh0511 <970252187@qq.com>
2025-08-11 13:38:24 +0800

    update example

--- examples/add_addr.rs
+++ examples/add_addr.rs
@@ -1,6 +1,6 @@
 use netconfig_rs::{ipnet, Interface};
 use std::error::Error;
-use std::net::{IpAddr, Ipv4Addr};
+use std::net::{Ipv4Addr, Ipv6Addr};
 fn main() -> Result<(), Box<dyn Error>> {
     let interface_name = "tun0";
     let interface = Interface::try_from_name(interface_name)?;
@@ -8,7 +8,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("before {:#?}", addrs);
     interface.add_address(ipnet::IpNet::new(Ipv4Addr::from([10, 6, 0, 1]).into(), 24)?)?;
     interface.add_address(ipnet::IpNet::new(Ipv4Addr::from([10, 5, 0, 1]).into(), 24)?)?;
+    interface.add_address(ipnet::IpNet::new(
+        Ipv6Addr::from([
+            0xCDCD, 0x910A, 0x2222, 0x5498, 0x8475, 0x1111, 0x3900, 0x2021,
+        ])
+        .into(),
+        24,
+    )?)?;
     let addrs = interface.addresses()?;
     println!("after {:#?}", addrs);
+    println!("remove addr:");
+    interface.remove_address(ipnet::IpNet::new(Ipv4Addr::from([10, 5, 0, 1]).into(), 24)?)?;
+    let addrs = interface.addresses()?;
+    println!("final {:#?}", addrs);
     Ok(())
 }
