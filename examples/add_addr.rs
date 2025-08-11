30167b39d17d2b7379d1f1f6fe3e9e5d464b60ef
xmh0511 <970252187@qq.com>
2025-08-11 11:08:06 +0800

    fix ipnet panic

--- examples/add_addr.rs
+++ examples/add_addr.rs
@@ -1,13 +1,13 @@
+use netconfig_rs::{ipnet, Interface};
 use std::error::Error;
 use std::net::{IpAddr, Ipv4Addr};
-use netconfig_rs::{Interface, ipnet};
-fn main()->Result<(),Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     let interface_name = "tun0";
     let interface = Interface::try_from_name(interface_name)?;
     let addrs = interface.addresses()?;
     println!("before {:#?}", addrs);
-    interface.add_address(ipnet::IpNet::new(Ipv4Addr::from([10,6,0,1]).into(),24)?)?;
-    interface.add_address(ipnet::IpNet::new(Ipv4Addr::from([10,5,0,1]).into(),24)?)?;
+    interface.add_address(ipnet::IpNet::new(Ipv4Addr::from([10, 6, 0, 1]).into(), 24)?)?;
+    interface.add_address(ipnet::IpNet::new(Ipv4Addr::from([10, 5, 0, 1]).into(), 24)?)?;
     let addrs = interface.addresses()?;
     println!("after {:#?}", addrs);
     Ok(())
