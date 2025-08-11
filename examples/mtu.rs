1c35e910a3931f958eae2ef67e239f9bd243ac85
xmh0511 <970252187@qq.com>
2025-08-11 14:27:49 +0800

    add example

---
+++ examples/mtu.rs
@@ -0,0 +1,41 @@
+use netconfig_rs::Interface;
+use std::error::Error;
+
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Index(IndexArgs),
+    Name(NameArgs),
+}
+#[derive(Args)]
+struct IndexArgs {
+    index: u32,
+}
+#[derive(Args)]
+struct NameArgs {
+    name: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+    let interface = match &cli.command {
+        Commands::Index(index) => Interface::try_from_index(index.index)?,
+        Commands::Name(name) => Interface::try_from_name(&name.name)?,
+    };
+    let mtu = interface.mtu()?;
+    println!("current mtu: {mtu}");
+    interface.set_mtu(1800)?;
+    let mtu2 = interface.mtu()?;
+    println!("after: {mtu2}");
+    interface.set_mtu(mtu)?;
+    Ok(())
+}
